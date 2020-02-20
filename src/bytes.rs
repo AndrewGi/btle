c03c2d9e1abc82ced03de71512a2bac32083657c
Andrew <andrew@gilbrough.com>
2020-02-19 23:35:43 -0800

    added StaticBuf type to bytes.rs.

--- src/bytes.rs
+++ src/bytes.rs
@@ -1,4 +1,6 @@
 use core::convert::TryInto;
+use core::ops;
+use std::ops::RangeFull;
 
 #[derive(Copy, Clone)]
 pub enum Endian {
@@ -183,5 +185,140 @@ impl ToFromBytesEndian for bool {
         } else {
             None
         }
+    }
+}
+
+/// Static byte buffer. `StaticBuf<[u8; 16]>` can store a `[u8]` array from 0-16 bytes for example.
+/// Unlike other static buffers, this does NOT reallocate if you out grow the internal buffer. If
+/// you try to request more bytes than its able to store, it will panic.  
+#[derive(Copy, Clone, Default)]
+pub struct StaticBuf<ArrayBuf: AsRef<[u8]> + Default + Copy> {
+    buf: ArrayBuf,
+    len: usize,
+}
+impl<ArrayBuf: AsRef<[u8]> + Default + Copy> StaticBuf<ArrayBuf> {
+    /// Returns the maximum size the `StaticBuf` can hold.
+    /// # Examples
+    /// ```
+    /// use btle::bytes::StaticBuf;
+    /// assert_eq!(StaticBuf::<[u8; 10]>::max_size(), 10);
+    /// assert_eq!(StaticBuf::<[u8; 23]>::max_size(), 23);
+    /// ```
+    pub fn max_size() -> usize {
+        ArrayBuf::default().as_ref().len()
+    }
+
+    /// Resizes the `StaticBuf` by settings `self.len` to `new_size` if `new_size <= Self::max_size()`.
+    /// This is only a single variable change and WILL NOT zero or change any of the buffers bytes.
+    /// # Examples
+    /// ```
+    /// use btle::bytes::{StaticBuf, Storage};
+    /// let mut buf = StaticBuf::<[u8; 10]>::with_size(10);
+    /// assert_eq!(buf.len(), 10);
+    /// assert_eq!(buf[9], 0);
+    /// buf[9] = 0xFF;
+    /// buf.resize(1);
+    /// assert_eq!(buf.len(), 1);
+    /// buf.resize(10);
+    /// assert_eq!(buf[9], 0xFF);
+    /// ```
+    pub fn resize(&mut self, new_size: usize) {
+        assert!(
+            new_size <= Self::max_size(),
+            "requested size {} bigger than static buf size {}",
+            new_size,
+            Self::max_size()
+        );
+        self.len = new_size;
+    }
+}
+impl<ArrayBuf: AsRef<[u8]> + Default + Copy> AsRef<[u8]> for StaticBuf<ArrayBuf> {
+    fn as_ref(&self) -> &[u8] {
+        &self.buf.as_ref()[..self.len]
+    }
+}
+impl<ArrayBuf: AsRef<[u8]> + AsMut<[u8]> + Default + Copy> AsMut<[u8]> for StaticBuf<ArrayBuf> {
+    fn as_mut(&mut self) -> &mut [u8] {
+        &mut self.buf.as_mut()[..self.len]
+    }
+}
+impl<ArrayBuf: AsRef<[u8]> + Default + Copy> ops::Index<ops::RangeFull> for StaticBuf<ArrayBuf> {
+    type Output = [u8];
+
+    fn index(&self, _index: RangeFull) -> &Self::Output {
+        self.as_ref()
+    }
+}
+impl<ArrayBuf: AsRef<[u8]> + AsMut<[u8]> + Default + Copy> ops::IndexMut<ops::RangeFull>
+    for StaticBuf<ArrayBuf>
+{
+    fn index_mut(&mut self, _index: RangeFull) -> &mut Self::Output {
+        self.as_mut()
+    }
+}
+impl<ArrayBuf: AsRef<[u8]> + Default + Copy> ops::Index<usize> for StaticBuf<ArrayBuf> {
+    type Output = u8;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.as_ref()[index]
+    }
+}
+
+impl<ArrayBuf: AsRef<[u8]> + AsMut<[u8]> + Default + Copy> ops::IndexMut<usize>
+    for StaticBuf<ArrayBuf>
+{
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.as_mut()[index]
+    }
+}
+impl<ArrayBuf: AsRef<[u8]> + Default + Copy> Storage for StaticBuf<ArrayBuf> {
+    fn with_size(size: usize) -> Self
+    where
+        Self: Sized,
+    {
+        assert!(
+            size <= Self::max_size(),
+            "requested size {} bigger than static buf size {}",
+            size,
+            Self::max_size()
+        );
+        Self {
+            buf: ArrayBuf::default(),
+            len: size,
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.len
+    }
+}
+
+/// Objects that store and own bytes (`Box<[u8]>`, `Vec<u8>`, `StaticBuf<[u8; 32]>`, etc).
+/// This allows for generic byte storage types for byte buffers.
+pub trait Storage: AsRef<[u8]> {
+    fn with_size(size: usize) -> Self
+    where
+        Self: Sized;
+    fn len(&self) -> usize {
+        self.as_ref().len()
+    }
+}
+impl Storage for Vec<u8> {
+    fn with_size(size: usize) -> Self
+    where
+        Self: Sized,
+    {
+        vec![0; size]
+    }
+    fn len(&self) -> usize {
+        <Vec<u8>>::len(self)
+    }
+}
+impl Storage for Box<[u8]> {
+    fn with_size(size: usize) -> Self
+    where
+        Self: Sized,
+    {
+        Vec::with_size(size).into_boxed_slice()
     }
 }
